7d2acea7eb8a4e9167dc321a1881a6adb5d58001
zfhrosyanail-pixel <zfhrosyanail@gmail.com>
2025-12-15 16:38:42 +0700

    Add files via upload

---
+++ hapusDataMhs.php
@@ -0,0 +1,13 @@
+<?php
+    //memanggil file pustaka fungsi
+    require "koneksi.php";
+
+    //memindahkan data kiriman dari form ke var biasa
+    $id=$_GET["kode"];
+
+    //membuat query hapus data
+    $sql="delete from mhs where id=$id";
+    mysqli_query($koneksi,$sql);
+    header("location:tampilDataMhs.php");
+?>
+
